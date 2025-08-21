13a526f854987547d6819784480878d8389dd736
Christian Wygoda <christian.wygoda@wygoda.net>
2025-08-21 20:08:05 +0200

    feat(wasm): add wasm setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { greet } from '~pkg/trabant.js';
+</script>
+
+<main>
+	<div>
+		<h1>Vite + Svelte + WebAssembly</h1>
+		<h2>{greet('The Friendly TL')}</h2>
+	</div>
+</main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), wasm()],
+	server: {
+		fs: {
+			allow: ['./pkg']
+		}
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
